c19c0f17801236b96b19687cbf855f0ccdfac35b
23Maykol <maykollitari1@gmail.com>
2026-06-20 12:38:05 -0500

    Add Google Search Console verification

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
     return (
-        <main className={inter.className}>
-            <Component {...pageProps} />
-        </main>
+        <>
+            <Head>
+                <meta
+                    name="google-site-verification"
+                    content="2dApVjZLNG7GlPIbBgiN_NBVTyfUPyexus6JDqq41t8"
+                />
+            </Head>
+
+            <main className={inter.className}>
+                <Component {...pageProps} />
+            </main>
+        </>
     );
 }
 
