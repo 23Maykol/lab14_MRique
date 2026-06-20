0f24cd662930dd97a688fac500b2d523a7ae9b18
23Maykol <maykollitari1@gmail.com>
2026-06-20 12:22:15 -0500

    Primer commit

---
+++ components/LargeComponent.js
@@ -0,0 +1,8 @@
+export default function LargeComponent() {
+    return (
+        <div>
+            <h2>Componente cargado dinámicamente</h2>
+            <p>Este componente utiliza lazy loading con Next.js.</p>
+        </div>
+    );
+}
