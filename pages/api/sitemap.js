2312ba773ab4e02db4075a302619673530a79324
23Maykol <maykollitari1@gmail.com>
2026-06-20 12:56:35 -0500

    Corregir sitemap

--- pages/api/sitemap.js
+++ pages/api/sitemap.js
@@ -1,14 +1,14 @@
-const BASE_URL = "https://mi-sitio.com";
+const BASE_URL = "https://lab14-m-rique.vercel.app";
 
 export default async function handler(req, res) {
-    const urls = ["/", "/blog", "/contacto"]; // Agrega más rutas dinámicamente
+    const urls = ["/"];
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${urls
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls
             .map((url) => `<url><loc>${BASE_URL}${url}</loc></url>`)
             .join("")}
-  </urlset>`;
+</urlset>`;
 
     res.setHeader("Content-Type", "text/xml");
     res.write(sitemap);
